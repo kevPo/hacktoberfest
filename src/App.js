4581c9a64c4dc0dd55578cdb697eddd013d51c93
Kevin Poston <kevposton@gmail.com>
2017-10-09 23:32:38 -0400

    working on events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import errorImage from './ghost.svg';
 import './App.css';
 
+class UserNameInput extends Component {
+  render() {
+    
+    function handleKeyPress(event) {
+      // event.stopPropagation();
+      // event.preventDefault();
+      alert('hit');
+      if(event.key == 'Enter'){
+        alert('enter press here! ')
+      }
+    }
+
+    return (
+      <input className="spooky-input" type="text" onKeyPress={this.handleKeyPress}/>
+    );
+  }
+}
+
+function SpookyError(props) {
+  if (!props.error)
+    return null;
+  else
+    return (
+      <div>
+        <img className="spooky-ghost" src={errorImage} alt="Error Logo"/>
+        <div className="spooky-error">{props.error}</div>
+      </div>
+    );
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: 'There was an error'
+    };
+  }
+
   render() {
     return (
-      <h1 className="spooky-text">Hacktoberfest</h1>
+      <div className="container">
+        <h1 className="spooky-text">Hacktoberfest</h1>
+        <UserNameInput />
+        <SpookyError error={this.state.error} />
+      </div>
     );
   }
 }
